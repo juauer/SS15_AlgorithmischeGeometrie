4fbc90c2131911e28d2317777aef4169ea7aa86c
Julius Auer <julius.auer@gmail.com>
2015-04-25 12:24:53 +0200

    Add code for BHD and point-in-polygon problem.

--- AlGeo/src/geometry/Polygon.java
+++ AlGeo/src/geometry/Polygon.java
@@ -1,5 +1,67 @@
 package geometry;
 
-public class Polygon {
+import java.util.LinkedList;
 
+public class Polygon {
+    private class BHDNode {
+        protected final BHDNode     left;
+        protected final BHDNode     right;
+        protected final LineSegment line;
+
+        public BHDNode(BHDNode left, BHDNode right, LineSegment line) {
+            this.left = left;
+            this.right = right;
+            this.line = line;
+        }
+    }
+
+    public final Point[]  points;
+    private final BHDNode bHDRoot;
+
+    public Polygon(Point... points) {
+        this.points = points;
+        LinkedList<BHDNode> p1 = new LinkedList<BHDNode>();
+        LinkedList<BHDNode> p2 = new LinkedList<BHDNode>();
+
+        for(int i = 0; i < points.length - 1; ++i)
+            p1.add(new BHDNode(null, null, new LineSegment(points[i], points[i + 1])));
+
+        p1.add(new BHDNode(null, null, new LineSegment(points[points.length - 1], points[0])));
+
+        while(p1.size() > 1) {
+            while(p1.size() > 1)
+                p2.add(new BHDNode(p1.get(0), p1.get(1), new LineSegment(p1.removeFirst().line.p1, p1.removeFirst().line.p2)));
+
+            if(p1.size() == 1)
+                p2.add(p1.removeFirst());
+
+            p1.addAll(p2);
+            p2.clear();
+        }
+
+        bHDRoot = p1.getFirst();
+    }
+
+    private boolean contains(Point point, BHDNode node) {
+        if(node.left == null && node.right == null)
+            return false;
+
+        boolean leftOfLeftChild = point.distanceTo(node.left.line) > C.E;
+        boolean leftOfRightChild = point.distanceTo(node.right.line) > C.E;
+
+        if(leftOfLeftChild && leftOfRightChild)
+            return false;
+
+        if(!leftOfLeftChild && !leftOfRightChild)
+            return true;
+
+        if(leftOfLeftChild)
+            return contains(point, node.left);
+
+        return contains(point, node.right);
+    }
+
+    public boolean contains(Point point) {
+        return contains(point, bHDRoot);
+    }
 }
--- AlGeo/src/geometry/Test.java
+++ AlGeo/src/geometry/Test.java
@@ -2,8 +2,10 @@ package geometry;
 
 public class Test {
     public static void main(String[] args) {
-        LineSegment l1 = new LineSegment(new Point(0, 0), new Point(1, 1));
-        LineSegment l2 = new LineSegment(new Point(0, 2), new Point(1, 2));
-        System.out.println(l1.getIntersection(l2));
+        Polygon p = new Polygon(new Point(5, 10), new Point(7, 10), new Point(9, 8), new Point(10, 6), new Point(9, 4),
+                new Point(7, 2), new Point(5, 2), new Point(3, 4), new Point(2, 6), new Point(3, 8));
+        System.out.println(p.contains(new Point(4, 8)));
+        System.out.println(p.contains(new Point(3, 8)));
+        System.out.println(p.contains(new Point(3, 9)));
     }
 }
