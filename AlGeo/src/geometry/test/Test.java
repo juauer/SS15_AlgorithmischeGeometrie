a3475aa796b74d720db44ce95bc397cdbcf5fb7e
Julius Auer <julius.auer@gmail.com>
2015-04-26 16:39:35 +0200

    Some more debugging.

--- AlGeo/src/geometry/test/Test.java
+++ AlGeo/src/geometry/test/Test.java
@@ -8,28 +8,35 @@ import java.awt.Color;
 
 public class Test {
     public static void main(String[] args) {
-        Frame frame = new Frame();
+        ub1();
+    }
+
+    public static void ub1() {
         Polygon p = new Polygon(new Point(4, 9), new Point(6, 9), new Point(8, 7), new Point(9, 5), new Point(8, 3),
                 new Point(6, 1), new Point(4, 1), new Point(2, 3), new Point(1, 5), new Point(2, 7));
+        Frame frame = new Frame();
         frame.drawPolygon(p, Color.RED);
 
         Point point1 = new Point(3, 7);
         Point point2 = new Point(2, 7);
         Point point3 = new Point(2, 8);
-        System.out.println(p.contains(point1));
-        System.out.println(p.contains(point2));
-        System.out.println(p.contains(point3));
         frame.drawPoint(point1, Color.BLUE);
         frame.drawPoint(point2, Color.GREEN);
-        frame.drawPoint(point3, Color.YELLOW);
+        frame.drawPoint(point3, Color.CYAN);
+        System.out.println(String.format("polygon contains %s (blue): %s", point1, p.contains(point1)));
+        System.out.println(String.format("polygon contains %s (green): %s", point2, p.contains(point2)));
+        System.out.println(String.format("polygon contains %s (cyan): %s", point3, p.contains(point3)));
 
         Line line1 = new Line(new Point(3, 1), new Point(9, 6));
         Line line2 = new Line(new Point(8.5, 1), new Point(8.5, 9));
-        Line line3 = new Line(new Point(9, 1), new Point(11, 7));
-        System.out.println(p.intersectionWith(line1));
-        System.out.println(p.intersectionWith(line2));
-        System.out.println(p.intersectionWith(line3));
+        Line line3 = new Line(new Point(9, 1), new Point(10, 7));
         frame.drawLine(line1, Color.BLUE);
         frame.drawLine(line2, Color.GREEN);
+        frame.drawLine(line3, Color.CYAN);
+        System.out.println(String.format("intersection of polygon and line1 (blue): %s", p.intersectionWith(line1)));
+        System.out.println(String.format("intersection of polygon and line2 (green): %s", p.intersectionWith(line2)));
+        System.out.println(String.format("intersection of polygon and line3 (cyan): %s", p.intersectionWith(line3)));
+
+        // frame.writeToFile("./../assignments/ub1/test.png");
     }
 }
