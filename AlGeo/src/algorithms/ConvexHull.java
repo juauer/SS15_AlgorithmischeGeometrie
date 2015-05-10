51dd8c9fecab08a99119add8c4cf98c94f965a90
Julius Auer <julius.auer@gmail.com>
2015-05-10 17:37:21 +0200

    Benutze eine sortierte Liste für Graham. Macht MyAnstieg und getRelevantPoints überflüssig (beides entfernt). Stattdessen wird nur einmal eine Liste mit Punkten angelegt und sortiert. Das Preprocessing mit der TreeMap lief auch in nlogn (Suchen auf einer TreeMap braucht logn - genausoviel wie beim Sortieren dieser Punkte verbraucht würde) somit hatte es keinen Vorteil mit getRelevantPoints ein preprocessing zu machen - dafür hat es den Code unnötig kompliziert gemacht. Kann alles rausfliegen, sry :)

--- AlGeo/src/algorithms/ConvexHull.java
+++ AlGeo/src/algorithms/ConvexHull.java
@@ -1,7 +1,7 @@
 package algorithms;
 
+import geometry.C;
 import geometry.LineSegment;
-import geometry.MyAnstieg;
 import geometry.Point;
 import geometry.Polygon;
 import geometry.Vector;
@@ -10,11 +10,10 @@ import geometry.test.Scene;
 
 import java.awt.Color;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.Comparator;
 import java.util.Iterator;
-import java.util.Map;
+import java.util.LinkedList;
 import java.util.Stack;
-import java.util.TreeMap;
 
 import mats.Mat2x2;
 
@@ -37,75 +36,51 @@ public class ConvexHull {
         return min;
     }
 
-    public static Map<MyAnstieg, Point> getRelevantPoints(Point... points) {
-        Point min = getMinY(points);
-        Map<MyAnstieg, Point> relevantPoints = new HashMap<MyAnstieg, Point>();
-
-        for(Point p : points) {
-
-            if(p != min) {
-
-                Double d = p.toPosition().substract(min.toPosition()).getMirroredAscent();
-                MyAnstieg m = new MyAnstieg(d);
-
-                if(Double.isInfinite(d)) {
-                    m = new MyAnstieg(Double.MAX_VALUE);
-                }
-
-                if(!relevantPoints.containsKey(m)) {
-                    relevantPoints.put(m, p);
-                }
-                else {
-                    Point cp = relevantPoints.get(m);
-                    double distanceold = cp.getX() * cp.getX() + cp.getY() * cp.getY();
-                    double distancenew = p.getX() * p.getX() + p.getY() * p.getY();
-
-                    if(distancenew > distanceold) {
-                        relevantPoints.remove(m);
-                        relevantPoints.put(m, p);
-                    }
-                }
-            }
-        }
-        return relevantPoints;
-    }
-
     public static Polygon grahamscan(Frame frame, Point... pointcloud) {
         // get minimum
         Point min = getMinY(pointcloud);
 
-        // select only relevant points from point cloud
-        Map<MyAnstieg, Point> relevantPoints = getRelevantPoints(pointcloud);
+        // sort by ascent first and distance second
+        LinkedList<Point> sortedPoints = new LinkedList<Point>();
 
-        // sort by ascent
-        Map<MyAnstieg, Point> sortedPoints = new TreeMap<MyAnstieg, Point>(Collections.reverseOrder());
-        for(MyAnstieg a : relevantPoints.keySet()) {
-            sortedPoints.put(a, relevantPoints.get(a));
-        }
+        for(Point p : pointcloud)
+            if(p != min)
+                sortedPoints.add(p);
+
+        Collections.sort(sortedPoints, new Comparator<Point>() {
+
+            @Override
+            public int compare(Point p1, Point p2) {
+                Vector pos1 = p1.toPosition().substract(min.toPosition());
+                Vector pos2 = p2.toPosition().substract(min.toPosition());
+                double ascent1 = pos2.getMirroredAscent();
+                double ascent2 = pos1.getMirroredAscent();
+
+                if(Math.abs(ascent1 - ascent2) < C.E) {
+                    double l = pos1.length() - pos2.length();
+                    return (int) (l < 0 ? Math.floor(l) : Math.ceil(l));
+                }
+
+                double d = ascent1 - ascent2;
+                return (int) (d < 0 ? Math.floor(d) : Math.ceil(d));
+            }
+        });
 
         // put first 3 points on stack
         Stack<Point> stack = new Stack<Point>();
         stack.push(min);
-
-        MyAnstieg ascent = ((TreeMap<MyAnstieg, Point>) sortedPoints).firstKey();
-        Point p = sortedPoints.get(ascent);
-        sortedPoints.remove(ascent);
-        stack.push(p);
-
-        ascent = ((TreeMap<MyAnstieg, Point>) sortedPoints).firstKey();
-        p = sortedPoints.get(ascent);
-        sortedPoints.remove(ascent);
-        stack.push(p);
+        stack.push(sortedPoints.removeFirst());
+        stack.push(sortedPoints.removeFirst());
 
         // have a look at each point
-        for(MyAnstieg ca : sortedPoints.keySet()) { // ca ... current ascent
+        for(Point cp : sortedPoints) { // ca ... current point
             Point t = stack.pop(); // t ... top
             Point ntt = stack.peek(); // ntt ... next-to-top
             stack.push(t); // put top back on stack
 
             // use determinant for testing left/right turns
             Vector v1 = new Vector(ntt.getX() - t.getX(), ntt.getY() - t.getY());
-            Vector v2 = new Vector(sortedPoints.get(ca).getX() - t.getX(), sortedPoints.get(ca).getY() - t.getY());
+            Vector v2 = new Vector(cp.getX() - t.getX(), cp.getY() - t.getY());
 
             Mat2x2 matrix = new Mat2x2(v1.get(0), v1.get(1), v2.get(0), v2.get(1));
 
@@ -113,7 +88,7 @@ public class ConvexHull {
             while(!(matrix.getDeterminant() <= 0)) {
                 if(frame != null) {
                     LineSegment line1 = new LineSegment(ntt, t);
-                    LineSegment line2 = new LineSegment(t, sortedPoints.get(ca));
+                    LineSegment line2 = new LineSegment(t, cp);
                     Scene s = new Scene(1000);
                     Point ptemp = min;
 
@@ -137,14 +112,14 @@ public class ConvexHull {
                 stack.push(t);
 
                 v1 = new Vector(ntt.getX() - t.getX(), ntt.getY() - t.getY());
-                v2 = new Vector(sortedPoints.get(ca).getX() - t.getX(), sortedPoints.get(ca).getY() - t.getY());
+                v2 = new Vector(cp.getX() - t.getX(), cp.getY() - t.getY());
 
                 matrix = new Mat2x2(v1.get(0), v1.get(1), v2.get(0), v2.get(1));
             }
 
             if(frame != null) {
                 LineSegment line1 = new LineSegment(ntt, t);
-                LineSegment line2 = new LineSegment(t, sortedPoints.get(ca));
+                LineSegment line2 = new LineSegment(t, cp);
                 Scene s = new Scene(1000);
                 Point ptemp = min;
 
@@ -160,7 +135,7 @@ public class ConvexHull {
             }
 
             // put current point on stack
-            stack.push(sortedPoints.get(ca));
+            stack.push(cp);
         }
 
         // generate polygon from points on stack
